15400bbb84d7811af29122c316bb8ff50ff7cbb9
Beneqi <obbonobo@gmail.com>
2024-01-05 00:30:34 +0600

    [EnableCors]

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -3,6 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(options => {
+	options.AddDefaultPolicy(cfg => {
+		cfg.WithOrigins(builder.Configuration["AllowedOrigins"]);
+		cfg.AllowAnyHeader();
+		cfg.AllowAnyMethod();
+	});
+	options.AddPolicy(name: "AnyOrigin",
+	cfg => {
+		cfg.AllowAnyOrigin();
+		cfg.AllowAnyHeader();
+		cfg.AllowAnyMethod();
+	});
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
