b725f6c6861dda8b6cf54e16bd5a5cd393aea35a
Beneqi <obbonobo@gmail.com>
2024-01-14 17:23:23 +0600

    paging and sorting

--- MyBGList/Controllers/BoardGamesController.cs
+++ MyBGList/Controllers/BoardGamesController.cs
@@ -4,40 +4,50 @@ using MyBGList.DTO;
 using System.Data.SqlTypes;
 using Microsoft.EntityFrameworkCore;
 using MyBGList.Models;
+using System.Linq.Dynamic.Core;
 
-namespace MyBGList.Controllers
+namespace MyBGList.Controllers;
+
+//[Route("api/[controller]")]
+[Route("[controller]")]
+[ApiController]
+public class BoardGamesController : ControllerBase
 {
-	//[Route("api/[controller]")]
-	[Route("[controller]")]
-	[ApiController]
-	public class BoardGamesController : ControllerBase
+	private readonly ApplicationDbContext _context;
+	private readonly ILogger<BoardGamesController> _logger;
+
+	public BoardGamesController(ApplicationDbContext context,
+		ILogger<BoardGamesController> logger)
 	{
-		private readonly ApplicationDbContext _context;
-		private readonly ILogger<BoardGamesController> _logger;
+		_context = context;
+		_logger = logger;
+	}
 
-		public BoardGamesController(ApplicationDbContext context,
-			ILogger<BoardGamesController> logger)
-		{
-			_context = context;
-			_logger = logger;
-		}
+	[HttpGet(Name = "GetBoardGames")]
+	[ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
+	public async Task<RestDTO<BoardGame[]>> Get(int pageIndex = 0, 
+		int pageSize = 10, 
+		string? sortColumn = "Name",
+		string? sortOrder = "ASC")
+	{
+		var query = _context.BoardGames
+			.OrderBy($"{sortColumn} {sortOrder}")
+			.Skip(pageIndex * pageSize)
+			.Take(pageSize);
 
-		[HttpGet(Name = "GetBoardGames")]
-		[ResponseCache(Location = ResponseCacheLocation.Any, Duration = 60)]
-		public async Task<RestDTO<BoardGame[]>> Get()
+		return new RestDTO<BoardGame[]>() 
 		{
-			var query = _context.BoardGames;
-			return new RestDTO<BoardGame[]>() 
+			Data = await query.ToArrayAsync(),
+			PageIndex = pageIndex,
+			PageSize = pageSize,
+			RecordCount = await _context.BoardGames.CountAsync(),
+			Links = new List<LinkDTO> 
 			{
-				Data = await query.ToArrayAsync(),
-				Links = new List<LinkDTO> 
-				{
-					new LinkDTO(
-						Url.Action(null, "BoardGames", null, Request.Scheme)!,
-						"self",
-						"GET"), 
-				}
-			};
-		}
+				new LinkDTO(
+					Url.Action(null, "BoardGames", new { pageIndex, pageSize }, Request.Scheme)!,
+					"self",
+					"GET"), 
+			}
+		};
 	}
 }
