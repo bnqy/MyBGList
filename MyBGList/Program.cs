560cb4ca96df6dd266e3b7eb3b8f69087356b0ac
Beneqi <obbonobo@gmail.com>
2024-01-28 20:53:20 +0600

    sqlserver distributed cache

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -113,6 +113,14 @@ builder.Services.AddResponseCaching(options =>
 
 builder.Services.AddMemoryCache();
 
+builder.Services.AddDistributedSqlServerCache(options =>
+{
+	options.ConnectionString =
+	builder.Configuration.GetConnectionString("DefaultConnection");
+	options.SchemaName = "dbo";
+	options.TableName = "AppCache";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
