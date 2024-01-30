dd302d8b75764fb315deb8faa9615ac4d4480beb
Beneqi <obbonobo@gmail.com>
2024-01-30 23:29:55 +0600

    author http header

--- MyBGList/Program.cs
+++ MyBGList/Program.cs
@@ -10,6 +10,7 @@ using Serilog.Sinks.MSSqlServer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -95,9 +96,35 @@ builder.Services.AddCors(options => {
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(options => {
 	options.ParameterFilter<SortColumnFilter>();
 	options.ParameterFilter<SortOrderFilter>();
+
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Description = "Please enter token",
+		Name = "Authorization",
+		Type = SecuritySchemeType.Http,
+		BearerFormat = "JWT",
+		Scheme = "bearer"
+	});
+
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type=ReferenceType.SecurityScheme,
+					Id="Bearer"
+				}
+			},
+			Array.Empty<string>()
+		}
+	});
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
