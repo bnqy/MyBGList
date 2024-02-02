e1cb050b0a958803d44c78dbebac35ace81861fb
Beneqi <obbonobo@gmail.com>
2024-02-02 16:36:20 +0600

    grpc client done

---
+++ MyBGList/Controllers/GrpcController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Grpc.Net.Client;
+using MyBGList.gRPC;
+
+
+namespace MyBGList.Controllers;
+
+[Route("[controller]/[action]")]
+[ApiController]
+public class GrpcController : ControllerBase
+{
+	[HttpGet("{id}")]
+	public async Task<BoardGameResponce> GetBoardGame(int id)
+	{
+		using var channel = GrpcChannel.ForAddress("https://localhost:40443");
+
+		var client = new gRPC.Grpc.GrpcClient(channel);
+
+		var responce = await client.GetBoardGameAsync(new BoardGameRequest { Id = id});
+
+		return responce;
+	}
+}
